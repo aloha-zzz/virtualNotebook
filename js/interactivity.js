53ad68ec717fff56ed20307367f31b87e8a7c049
zhou haoxiang <835390899@qq.com>
2017-09-16 13:09:18 +0800

    界面 基本差不多  自适应还没弄

--- js/interactivity.js
+++ js/interactivity.js
@@ -12,6 +12,37 @@ s('closeDeleteDiv').onclick=function () {
     s('deleteDiv').style.display='none';
 }
 var manage=document.querySelectorAll('.manage');
-for(var i=0;i<manage.length;i++){
 
+var touch=document.querySelectorAll('.touch');
+for(var i=0;i<touch.length;i++){
+    (function (g) {
+        touch[g].onclick=function (e) {
+            console.log(e.clientX+' '+e.clientY)
+            s('tools').style.display='none'
+            s('getTask').style.display='block';
+            s('getTask').style.left=e.clientX+'px';
+            s('getTask').style.top=e.clientY+'px';
+            s('selected').style.display='block';
+            s('selected').style.left=e.clientX-40+'px';
+            s('selected').style.top=e.clientY-40+'px';
+            for(var i=0;i<manage.length;i++){
+                (function (g) {
+                    manage[g].onmouseover=function () {
+                        console.log(g)
+                        console.log(manage[g].parentNode)
+                        manage[g].parentNode.innerHTML='<img class="manage" src="img/manage_fill.png">'
+                        s('tools').style.display='flex';
+                        s('tools').style.top=e.clientY+65+60*g+'px'
+                        s('tools').style.left=e.clientX+335+'px'
+                    }
+                    manage[g].onmouseout=function () {
+                        console.log(111)
+                        // manage[g].parentNode.innerHTML='<img class="manage" src="img/manage.png">'
+                        setTimeout('s(\'tools\').style.display=\'none\'',10000);
+                    }
+
+                })(i)
+            }
+        }
+    })(i)
 }
